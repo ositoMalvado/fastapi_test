78728e628fa69a96d055a1f71d3c5d3cc0d1e10e
ositoMalvado <mordecaaii@gmail.com>
2024-05-06 20:44:12 -0300

    first release

---
+++ main.py
@@ -0,0 +1,16 @@
+﻿import flet as ft
+import flet_fastapi
+
+
+class TextInput(ft.TextField):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+async def main(page: ft.Page):
+    await page.add_async(
+        TextInput(label="Hello, Flet!"),
+        TextInput(label="Hello, Flet!")
+    )
+
+app = flet_fastapi.app(main)
